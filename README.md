a3c272c9ea68536a7f131cdc5dc9178419b27820
Bill <klinkow@gmail.com>
2016-11-18 13:21:07 -0800

    updated readme.md to include link, refactored specs

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ _This web app will take a master word from the user as well as text to search th
 ## Setup/Installation Requirements
 
 * _This web app will run on most browsers_
-* [Link to Website](https://klinkow.github.io/project)
+* [https://blooming-eyrie-62934.herokuapp.com/](Word_Count)
 
 ## Support and contact details
 
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -4,11 +4,11 @@ require('./lib/word_count')
 describe('Class#word_count') do
 
   it('will take a single word input, check if it matches the argument word and output a wordcount of 1') do
-    expect('cat').word_count('cat')to(eq('1'))
+    expect('cat'.word_count('cat')).to(eq(1))
   end
 
   it('will take a multiple-word input, check if any of the words match the argument word and update the counter for each') do
-    expect('cat dog cat').word_count('cat')to(eq('2'))
+    expect('cat dog cat'.word_count('cat')).to(eq(2))
   end
 
 end
