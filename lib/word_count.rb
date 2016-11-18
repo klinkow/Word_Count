6154f5cb63537880a3ee918945c947a84beb0af9
Bob <klinkow@gmail.com>
2016-11-18 09:07:23 -0800

    develop word_count method to pass second spec

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,8 +1,11 @@
 class String
   define_method(:word_count) do |master_word|
     counter = 0
-    if .include("master_word") === true
-      counter += 1
+    input_as_word_array = self.split(" ")
+    input_as_word_array.each() do |word|
+      if word.include("master_word") === true
+        counter += 1
+      end
     end
     counter
   end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,8 +2,13 @@ require('rspec')
 require('./lib/word_count')
 
 describe('Class#word_count') do
+
   it('will take a single word input, check if it matches the argument word and output a wordcount of 1') do
     expect('cat').word_count('cat')to(eq('1'))
+  end
+
+  it('will take a multiple-word input, check if any of the words match the argument word and update the counter for each') do
+    expect('cat dog cat').word_count('cat')to(eq('2'))
   end
 
 end
